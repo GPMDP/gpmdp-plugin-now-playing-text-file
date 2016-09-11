570fa2a671400c73e9b65996e9ff7db7a256e214
Samuel Attard <samuel.r.attard@gmail.com>
2016-09-12 06:57:53 +1000

    Update to include settings

--- src/index.js
+++ src/index.js
@@ -3,28 +3,41 @@ import path from 'path';
 
 class NowPlayingTextFilePlugin {
   static PLUGIN_NAME = 'Now Playing Text File';
+  static CONFIG = [
+    {
+      key: 'file_name',
+      name: 'File Name',
+      default: 'now_playing.txt',
+      type: 'text',
+    },
+    {
+      key: 'enabled',
+      name: 'Enabled',
+      default: true,
+      type: 'bool',
+    },
+  ];
 
-  constructor(electronRequire, Emitter, PlaybackAPI, WindowManager, buildDir, additionals) {
+  constructor(electronRequire, Emitter, Settings, PlaybackAPI, WindowManager, buildDir, additionals) {
     this.require = electronRequire;
     this.Emitter = Emitter;
+    this.Settings = Settings;
     this.PlaybackAPI = PlaybackAPI;
     this.WindowManager = WindowManager;
     this.buildDir = buildDir;
     this.additionals = additionals;
     this.isMainProcess = PlaybackAPI !== null;
-
-    if (this.isMainProcess) {
-      this.filePath = path.resolve(this.require('electron').app.getPath('userData'), 'now_playing.txt');
-    }
   }
 
   install() {
-    fs.writeFileSync(this.filePath, '');
+    if (this.isMainProcess) {
+      fs.writeFileSync(this._getFilePath(), '');
+    }
   }
 
   uninstall() {
-    fs.unlinkSync(this.filePath);
     if (this.isMainProcess) {
+      fs.unlinkSync(this._getFilePath());
       this.PlaybackAPI.removeListener('change:track', this._writeFile);
     }
   }
@@ -33,15 +46,33 @@ class NowPlayingTextFilePlugin {
     if (this.isMainProcess) {
       this.PlaybackAPI.on('change:track', this._writeFile);
       this._writeFile();
+      this.lastPath = this._getFilePath();
+      this.Settings.onChange('enabled', (enabled) => {
+        if (enabled) {
+          this._writeFile();
+        } else {
+          fs.writeFileSync(this._getFilePath(), '');
+        }
+      });
+      this.Settings.onChange('file_name', (newFileName) => {
+        if (fs.existsSync(this.lastPath)) fs.unlinkSync(this.lastPath);
+        this.lastPath = this._getFilePath();
+        this._writeFile();
+      });
     }
   }
 
+  _getFilePath() {
+    return path.resolve(this.require('electron').app.getPath('userData'), this.Settings.get('file_name'));
+  }
+
   _writeFile = () => {
+    if (!this.Settings.get('enabled')) return;
     const track = this.PlaybackAPI.currentSong(true);
     if (track) {
-      fs.writeFileSync(this.filePath, `${track.title} - ${track.artist}`);
+      fs.writeFileSync(this._getFilePath(), `${track.title} - ${track.artist}`);
     } else {
-      fs.writeFileSync(this.filePath, '');
+      fs.writeFileSync(this._getFilePath(), '');
     }
   }
 }
